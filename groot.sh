ec1b97a107dd31876039c780f99d67eab28cfc4a
Borrego Johnson <borregs@users.noreply.github.com>
2017-11-27 19:42:16 -0800

    Update groot.sh

--- groot.sh
+++ groot.sh
@@ -1,17 +1,17 @@
-:"
- GRAPICS ENABLED CHROOT SCRIPT
+:'
+ GRAPICS ENABLED CHROOT SCRIPTb
  Ubuntuish
-:"
+'
 # Mount ur device prior to running this
 # this is usually done by a graphical application
 # but mount methods vary for diferent media and fmts
 
 GROOTP="<path/where/media/mounted>"
 
-sudo mount --bind /dev $GROOTD/dev
+sudo mount --bind /dev $GROOTPb/dev
 sudo mount --bind /tmp $GROOTP/tmp
 sudo mount --bind /sys $GROOTP/sys
-sudo mount --bind /proc $GROOTP/proc
+sudo mount --bind /proc $GROOTP/procb
 sudo mount --bind /etc $GROOTP/etc
 
  
